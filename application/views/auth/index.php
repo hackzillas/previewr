8bd801dafebd3d8306796ede5dfc265c2ba4a285
Thomas Bishop <thomas@bishop-creative.com>
2012-02-04 17:26:27 -0600

    Setup landing page and configured Bootstrap further

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -1,1 +1,11 @@
-This is the auth page...
+<div class="row form-login">
+	<div class="span4 offset4">
+		<h1 class="logo">Preview App</h1>
+		<form id="home_signup" name="home_signup" action="#" method="post">
+			<p><label for="email">Email</label><input type="text" name="email" id="email" /></p>
+			<p><label for="password">Password</label><input type="text" name="password" id="password" /></p>
+			<p><input type="submit" name="submit" value="submit" class="btn btn-large btn-primary" /></p>
+		</form>
+	</div>
+</div>
+	
