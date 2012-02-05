4890ab66271bc8bbb0ab2d4377193560c0e5fd63
Chris Schmitz <ccschmitz@gmail.com>
2012-02-04 20:44:49 -0600

    Added form validation on the login screen

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -11,7 +11,19 @@ class Auth_Controller extends Base_Controller {
 
 	public function post_login()
 	{
-		
+		$rules = array(
+			'email' => array('required', 'email', 'max:100'),
+			'password' => array('required', 'min:6')
+		);
+
+		$validator = Validator::make(Input::all(), $rules);
+
+		if ($validator->valid())
+		{
+			return Redirect::to('projects');
+		}
+
+		return Redirect::to('/')->with_input();
 	}
 
 }
--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -3,9 +3,9 @@
 		<h1 class="logo">Preview</h1>
 
 		<?php echo Form::open('auth/login', 'POST'); ?>
-			<p><?php echo Form::text('email', '', array('placeholder' => 'Email')); ?></p>
+			<p><?php echo Form::text('email', Input::old('email'), array('placeholder' => 'Email')); ?></p>
 			<p><?php echo Form::password('password', array('placeholder' => 'Password')); ?></p>
-			<p><?php echo Form::submit('submit', array('class' => 'btn btn-large btn-primary')); ?></p>
+			<p><?php echo Form::submit('Log In', array('class' => 'btn btn-large btn-primary')); ?></p>
 		<?php echo Form::close(); ?>
 		<p><?php echo HTML::link('#', 'Forget your password?'); ?></p>
 	</div>
