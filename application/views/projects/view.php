538be595157d9dd8a1957a7332ea607c1a68f50a
Chris Schmitz <ccschmitz@gmail.com>
2012-02-05 22:55:33 -0600

    Setup the main projects view

--- application/views/projects/view.php
+++ application/views/projects/view.php
@@ -1,1 +1,68 @@
-<?php echo $project; ?>
+<div id="content" class="row">
+
+	<aside id="sidebar" class="span3">
+		<div class="widget widget-user-info">
+			<h2 class="widget-title">Account</h2>
+			<div class="body">
+				<p><img src="http://placehold.it/100x100" class="profile-photo" alt="" /></p>
+				<h3>Ben Kenobi</h3>
+				<p class="title">Jedi Extraordinaire</p>
+				<div id="user-stats">
+				<div class="btn-group">
+					<a href="#" class="btn">
+						38 Projects
+					</a>
+					<a href="#" class="btn">
+						320 Previews
+					</a>
+				</div>
+			</div>
+		</div>
+		</div>
+	</aside>
+
+	<section id="main" class="span9 grid">
+		<h1 class="inline-block title">Previews</h1>
+		<div class="btn-group right">
+			<a class="btn" rel="tooltip" href="<?php echo URL::to('projects/new'); ?>" data-original-title="New Project"><i class="icon-plus"></i><div class="hidden">Create a New Project</div></a>
+			<a class="btn" rel="tooltip" href="<?php echo URL::to('projects/archived'); ?>" data-original-title="Archived Projects"><i class="icon-folder-close"></i><div class="hidden">View Archived Projects</div></a>
+		</div>
+		<div class="btn-group right account">
+			<a class="btn" href="#">Account</a>
+			<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+			<span class="caret"></span>
+			</a>
+			<ul class="dropdown-menu">
+				<li class=""><a href="<?php echo URL::to('projects'); ?>">Projects</a></li>
+				<li class=""><a href="<?php echo URL::to('previews'); ?>">Previews</a></li>
+				<li class="divider"></li>
+				<li class=""><a href="<?php echo URL::to('auth/logout'); ?>">Log Out</a></li>
+		 	 </ul>
+		</div>
+		
+		<div class="row">
+		<?php foreach($previews as $preview): ?>
+		<?php $preview = $preview->attributes; ?>
+			<div class="item span3">
+				<a href="<?php echo URL::to('previews/'.$preview['id']); ?>">
+					<img src="http://placehold.it/400x300" alt="<?php echo $preview['name']; ?>" />
+				</a>
+			</div>
+		<?php endforeach; ?>
+		</div>
+		
+		<div class="pagination">
+			<ul>
+				<li><a href="#">Prev</a></li>
+				<li class="active">
+					<a href="#">1</a>
+				</li>
+				<li><a href="#">2</a></li>
+				<li><a href="#">3</a></li>
+				<li><a href="#">4</a></li>
+				<li><a href="#">Next</a></li>
+			</ul>
+		</div>
+	</section>
+
+</div>
