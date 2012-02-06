2f1dd3c98c372c202c8a89d057157f2b3c1d322a
Chris Schmitz <ccschmitz@gmail.com>
2012-02-05 22:55:45 -0600

    Setup the versions routes and views

---
+++ application/routes/versions.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Display the form to create a new version
+ */
+Router::register('GET /versions/new/(:num)', function()
+{
+	return View::make('layouts.default')
+		->nest('content', 'versions.new');
+});
+
+View::composer('versions.new', function($view)
+{
+	$view['preview'] = Preview::find(URI::segment(3));
+});
+
+/**
+ * Create the version
+ */
+Router::register('POST /versions/create/(:num)', function($preview_id)
+{
+	$rules = array(
+		'name' => array('required', 'min:4', 'max:100'),
+		'description' => array('min:10'),
+		'image' => array('required', 'mimes:jpeg,jpg,png,gif'),
+	);
+	$validator = Validator::make(Input::all(), $rules);
+
+	if ($validator->valid())
+	{
+		$image_path = path('public').'/uploads/versions/'.Input::file('image.name');
+
+		// upload the image
+		Input::upload('image', $image_path);
+
+		// create a new version
+		$version = new Version;
+
+		$version->preview_id = $preview_id;
+		$version->name = Input::get('name');
+		$version->description = Input::get('description');
+		$version->image_src = Input::file('image.name');
+
+		$version->save();
+
+		return Redirect::to('previews/'.$preview_id)
+			->with('message_success', 'Version created successfully!');
+	}
+
+	die('dead');
+	return Redirect::to('versions/new/'.$preview_id)
+		->with_input()
+		->with_errors($validator);
+});
