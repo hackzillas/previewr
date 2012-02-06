e32a9eb5595e4250f37be22db05c21402b1d107e
Chris Schmitz <ccschmitz@gmail.com>
2012-02-05 20:58:58 -0600

    Registered the main routes for previews

--- application/routes/previews.php
+++ application/routes/previews.php
@@ -1,7 +1,51 @@
-<?php 
+<?php
 
-Router::register('GET /previews', function()
+/**
+ * View a preview
+ */
+Router::register('GET /previews/(:num)', function($preview_id)
+{
+	// check to see if the project is protected
+
+	return View::make('layouts.default')
+		->with('preview', 'the preview content')
+		->nest('content', 'previews.view');
+});
+
+/**
+ * Display the create a preview form
+ */
+Router::register('GET /previews/new/(:num)', function($project_id)
 {
 	return View::make('layouts.default')
-		->nest('content', 'previews.view');
+		->with('project', 'the project content')
+		->nest('content', 'previews.new');
+});
+
+/**
+ * Save a new preview
+ */
+Router::register('POST /previews/create/(:num)', function($project_id)
+{
+	$rules = array(
+		'name' => array('required', 'min:4'),
+		'description' => array('min:10'),
+	);
+	$validator = Validator::make(Input::all(), $rules);
+
+	if ($validator->valid())
+	{
+		$preview = new Preview;
+
+		$preview->project_id = $project_id;
+		$preview->name = Input::get('name');
+		$preview->description = Input::get('description');
+
+		$preview->save();
+
+		return Redirect::to('previews/'.$project_id)
+			->with('message_success', 'Preview created successfully!');
+	}
+
+	return Redirect::to('previews/new');
 });
