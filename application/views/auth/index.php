44e712d45e1ef57bd3bdb58e99092d29570dc412
Thomas Bishop <thomas@bishop-creative.com>
2012-02-09 00:44:02 -0600

    New app name on auth index

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -1,6 +1,6 @@
 <div class="row form-login">
 	<div class="span4 offset4">
-		<h1 class="logo">Preview</h1>
+		<h1 class="logo">Previewr</h1>
 
 		<?php echo Form::open('auth/login', 'POST'); ?>
 			<p><?php echo Form::text('email', Input::old('email'), array('placeholder' => 'Email')); ?></p>
