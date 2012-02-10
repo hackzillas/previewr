d0a71cdc3d73da472277eb26c124aa2b44ea4114
Chris Schmitz <ccschmitz@gmail.com>
2012-02-09 22:40:44 -0600

    Added dynamic class to the body

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -20,5 +20,4 @@
 		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
 	</head>
-
-	<body data-spy="scroll" data-target=".subnav" data-offset="50">
+	<body data-spy="scroll" data-target=".subnav" data-offset="50" class="<?php echo str_replace('/', '-', URI::segment(1)); ?>">
