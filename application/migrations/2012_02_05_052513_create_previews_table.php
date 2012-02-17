0036e52815960d2a6dfc45fd461605f5a3d9ed36
Chris Schmitz <ccschmitz@gmail.com>
2012-02-17 00:54:48 -0600

    Added version_id to the previews table migration

--- application/migrations/2012_02_05_052513_create_previews_table.php
+++ application/migrations/2012_02_05_052513_create_previews_table.php
@@ -17,6 +17,7 @@ class Create_Previews_Table {
 			$table->integer('project_id');
 			$table->string('name');
 			$table->string('description');
+			$table->int('version_id');
 			$table->integer('preview_status_id');
 			$table->timestamps();
 		});
