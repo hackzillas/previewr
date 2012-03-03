a60cf8028a328bba62ae2b3d70763781cf2be687
Chris Schmitz <ccschmitz@gmail.com>
2012-03-02 19:12:36 -0600

    Removed shiv comment

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -7,7 +7,6 @@
 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
 		<!--[if lt IE 9]>
 			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
